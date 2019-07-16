2fe009c2f591b65970952856dc3feeca0a1e83a0
Sébastien <snx.dubois@gmail.com>
2019-07-16 15:55:45 +0200

    jpa init

---
+++ walkmybuddy/src/main/java/com/checkpoint4/walkmybuddy/WalkmybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.checkpoint4.walkmybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkmybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkmybuddyApplication.class, args);
+	}
+
+}
---
+++ walkmybuddy/src/test/java/com/checkpoint4/walkmybuddy/WalkmybuddyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.checkpoint4.walkmybuddy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalkmybuddyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
